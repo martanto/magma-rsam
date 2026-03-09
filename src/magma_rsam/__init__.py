68bcec8148756541a02d0b3b20ef5fd3228a131e
Martanto <martanto@live.com>
2026-03-09 18:31:47 +0100

    Init v2.0

--- src/magma_rsam/__init__.py
+++ src/magma_rsam/__init__.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from magma_rsam.rsam import RSAM
-from magma_rsam.plot_rsam import PlotRsam
-from pkg_resources import get_distribution
+from importlib.metadata import version
 
-__version__ = get_distribution("magma-rsam").version
+__version__ = version("magma-rsam")
 __author__ = "Martanto"
 __author_email__ = "martanto@live.com"
 __license__ = "MIT"
-__copyright__ = "Copyright (c) 2024, MAGMA Indonesia"
+__copyright__ = "Copyright (c) 2024, Martanto"
 __url__ = "https://github.com/martanto/magma-rsam"
-
-__all__ = [
-    "RSAM",
-    "PlotRsam",
-]
